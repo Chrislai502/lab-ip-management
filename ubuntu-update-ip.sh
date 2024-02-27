62da6fb690d2106817be3fdb9dca015eda1f4bb6
chrislai502 <wxunlai@gmail.com>
2024-02-27 14:27:19 -0800

    Updating Ubuntu Script

--- ubuntu-update-ip.sh
+++ ubuntu-update-ip.sh
@@ -6,7 +6,7 @@ TOKEN="roarmeow" # Your secret token for authentication
 
 # Use the -H flag to add headers for Content-Type and Authorization
 # Use the --data flag to send the payload as JSON
-curl -X POST "https://worker-ip-update.wxunlai.workers.dev/update" \
+curl -X POST "https://minion-ip-update.wxunlai.workers.dev/update" \
      -H "Authorization: Bearer $TOKEN" \
      -H "Content-Type: application/json" \
      --data "{\"uniqueName\":\"$UNIQUE_NAME\", \"username\":\"$USERNAME\", \"ip\":\"$IP\"}"
